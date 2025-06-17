93fed7fe6f04253941bdcf31f679abbf5c4b081d
aldeop <alex.vern13@gmail.com>
2025-06-17 16:38:00 +0100

    Create public subnets and associate public route table

--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC."
   default     = "10.0.0.0/16"
 }
+
+variable "availability_zones" {
+  description = "A list of availability zones to use for the subnets."
+  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "A list of CIDR blocks for the public subnets."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
