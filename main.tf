cc20c82f8a29e6963e088b270f5368a850171668
aldeop <alex.vern13@gmail.com>
2025-06-17 16:35:24 +0100

    Provision VPC and Internet Gateway

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "igw"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
